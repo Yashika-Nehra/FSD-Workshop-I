fab8321a689a43b466841d590004c04a80eb8d49
Yashika-Nehra <nehrayashika767@gmail.com>
2026-07-22 11:09:17 +0530

    basic javascript

---
+++ day.js
@@ -0,0 +1,31 @@
+let age=23;
+console.log(age);
+
+const c="Hello, World!";
+console.log(c);
+
+//datatype
+console.log(typeof age);
+console.log(typeof c);
+console.log(typeof true);
+console.log(typeof null);
+
+let a;
+let b=null;
+console.log(typeof a); //undefined
+console.log(typeof b); //object
+
+let person={
+    name:"John",
+}
+console.log(typeof person); //object
+console.log(person.name); //John
+
+//operators
+let x=10;
+let y=5;
+console.log(x+y); //15
+console.log(x-y); //5
+console.log(x*y); //50
+console.log(x/y); //2
+console.log(x%y); //0
